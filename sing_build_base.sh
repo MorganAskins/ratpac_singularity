a8f0738397bbd8643bd5ba648cf843f6c0eb9fbd
morgan <maskins@ucdavis.edu>
2018-11-26 09:32:47 -0800

    hi

--- sing_build_base.sh
+++ sing_build_base.sh
@@ -18,4 +18,4 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-singularity build wmbase.sif Singularity.base
+singularity --debug build wmbase.sif Singularity.base
--- sing_build_final.sh
+++ sing_build_final.sh
@@ -18,4 +18,4 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-singularity --debug build wmfinal.sif Singularity.ratpac
+singularity build wmfinal.sif Singularity.ratpac
